73595773cc81cd3449fa54b4059c9ac2593c295c
CaoQian <afewgoodken@icloud.com>
2018-09-19 14:44:57 +0800

    first commit

---
+++ DogezaApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogezaApps
+//
+//  Created by CaoQian on 2018/09/19.
+//  Copyright © 2018 CaoQian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
